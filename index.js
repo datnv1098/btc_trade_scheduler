b22999dbc440e1a062fbe8c3ff04ca2fb56310e0
nrx2-home <datnv1098@gmail.com>
2024-11-29 19:16:19 +0700

    Update sendTelegramMessage Start server

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ const moment = require("moment");
 const {BTCSchedulerCronJob} = require("./btc_trade_scheduler.js");
 const {ETHSchedulerCronJob} = require("./eth_trade_scheduler");
 const {getChannelId} = require("./telegram/getChannelId");
+const {sendTelegramMessage} = require("./telegram/sendMessage");
 
 function runMain() {
   const systemTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
-  console.log('System Time Zone:', systemTimeZone);
   console.log("Main Start Time: ", moment().format('YYYY-MM-DD HH:mm:ss'));
   getChannelId().then()
+  sendTelegramMessage(`Start server\nSystem TimeZone: ${systemTimeZone}\n${moment().format('DD/MM/YYYY HH:mm:ss')}`).then()
   BTCSchedulerCronJob();
   ETHSchedulerCronJob();
 }
